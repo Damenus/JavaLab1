232b854d4008415c1be1c1055f19bf1e30ed53e8
Damenus <darczuk.damian@gmail.com>
2016-03-11 22:11:53 +0100

    Duży Piątek
    przepisano z laborki, dodano to co wiedziałem

--- src/javalab1/DiskFile.java
+++ src/javalab1/DiskFile.java
@@ -7,24 +7,93 @@ package javalab1;
 
 import java.io.File;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  *
  * @author Damian Darczuk
  */
-public class DiskFile implements Serializable, Comparable<DiskFile> {
-    private Set<DiskFile> files;
-    private int date; 
-    private String name; 
+public class DiskFile implements Serializable, Comparable<DiskFile> {    
     
+    public enum Type {
+        Katalog,
+        Plik;
+    }    
+    
+    private Set<DiskFile> Files;
+    private Date date; 
+    private String name;
+    private Type type;
+    
+    public DiskFile() {        
+    }
+    
+    public DiskFile(String sciezka, int tryb) {
+        File file = new File(sciezka);
+        
+        this.name = file.getName();
+        this.date.setTime(file.lastModified());
+        
+        if (file.isFile())
+             this.type = type.Plik;
+        else if (file.isDirectory()) {
+            this.type = type.Katalog;
+            
+            File[] filesArray = file.listFiles();
+            
+            if (tryb == 1) {
+                Files = new TreeSet();
+            }
+            else if (tryb == 2) {
+                Files = new HashSet();
+            }
+            
+            for(File f : filesArray) {
+                Files.add(new DiskFile(f.getAbsolutePath(),tryb) );
+                   
+            }
+            
+            
+         }    
+        
+    }
+    
+    
+    public void wypisz() {
+        
+        System.out.println("-");   
+        System.out.println(this.getName());   
+        
+        if (this.getType() == type.Katalog)
+             System.out.println("\tK\t");
+        else
+            System.out.println("\tP\t");
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd");
+        String formatedDate = sdf.format(date);
+        System.out.println(formatedDate);        
+        
+    }
+    
+    Type getType() {
+        return this.type;
+    }
+    
+    String getName() {
+        return this.name;
+    }
     
      @Override
     public int hashCode() {
         int hash = 7;
         hash = 89 * hash + Objects.hashCode(this.name);
-        hash = 89 * hash + this.date;
+       // hash = 89 * hash + this.date;
         return hash;
     }
 
--- src/javalab1/JavaLab1.java
+++ src/javalab1/JavaLab1.java
@@ -5,6 +5,11 @@
  */
 package javalab1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Damian Darczuk
@@ -18,11 +23,37 @@ public class JavaLab1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        int tryb = 1;
+        String sciezka = "tak";
+        boolean czyPoprawne = false;
+        Scanner odczyt = new Scanner(System.in); 
+
+ 
+        while(!czyPoprawne) {  
+            try {
+                tryb = odczyt.nextInt();            
+                sciezka = odczyt.next();
+                if (!new File(sciezka).exists()) {
+                   throw  new FileNotFoundException();
+                }
+                czyPoprawne = true;
+            }
+            catch(FileNotFoundException e ) { 
+                System.out.println("Niepoprawna ścieżka. Wpisz poprawną! ");
+            }
+            catch (NumberFormatException n) { System.out.println("Niepoprawne dane! " +
+                     "\n Który element tablicy chcesz zobaczyć: ");
+            }       // zmienić wyjątek 
+        }
+            
+       System.out.println("Wybrałeś tryb: " + tryb + " Dla ścieżki: " + sciezka); 
+       DiskFile file = new DiskFile(sciezka, tryb);
+       file.wypisz();
         
 
-    Constants c = Constants.CONSTANT_1;
+    
     }
     
 }
